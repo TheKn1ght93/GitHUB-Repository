d9d8dce919282cac1da4fe16ddafa2ad378b9963
Radu Filimon <filimonradu@hotmail.com>
2019-12-04 12:08:46 +0200

    app add

---
+++ Load Option/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Load_Option
+{
+       
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        string pu_DT,
+            pu_CS,
+            de_DT,
+            de_CS,
+            dh,
+            loadDis,
+            mapLink,
+            commodity,
+            weight,
+            gross,
+            mc,
+            name,
+            phone,
+            broker,
+            addInfo;
+
+
+        private void Option_Click(object sender, RoutedEventArgs e)
+        {
+            Option option = new Option();
+            option.Show();         
+
+            pu_DT = pu_DT_Label.Content + pu_DT_TB.Text;
+            pu_CS = pu_CS_Label.Content + pu_CS_TB.Text;
+            de_DT = de_DT_Label.Content + de_DT_TB.Text;
+            de_CS = de_CS_Label.Content + de_DT_TB.Text;
+            dh = dh_Label.Content + dh_TB.Text;
+            loadDis = load_Distance_Label.Content + loadDistance_TB.Text;
+            mapLink = map_Link_Label.Content + map_Link_TB.Text;
+            commodity = commodity_label.Content + commodity_TB.Text;
+            weight = weight_label.Content + weight_TB.Text;
+            gross = gross_label.Content + gross_TB.Text;
+            mc = mc_label.Content + mc_TB.Text;
+            name = name_Label.Content + name_TB.Text;
+            phone = phone_Label.Content + phone_TB.Text;
+            broker = broker_Label.Content + broker_TB.Text;
+            addInfo = addInfo_Label.Content + add_Info_TB.Text;
+
+            option.text_Option(pu_DT, pu_CS, de_DT, de_CS, dh, loadDis, mapLink, commodity,
+                weight, gross, mc, name, phone, broker, addInfo);
+        }
+    }
+   
+
+}
---
+++ Load Option/Option.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Load_Option
+{
+ 
+    
+    public partial class Option : Window
+    {
+       
+        public Option()
+        {
+            InitializeComponent();
+        }
+        
+        public void text_Option(string pu_DT, string pu_CS, string de_DT, string de_CS, string dh, string loadDis, string mapLink, string commodity, string weight, 
+            string gross, string mc, string name, string phone, string broker, string addInfo)
+        {
+            loadText.Text = pu_DT + "\n" + 
+                            pu_CS + "\n" + 
+                            de_DT + "\n" + 
+                            dh + "\n" + 
+                            loadDis + "\n" + 
+                            mapLink + "\n" + 
+                            commodity + "\n" + 
+                            weight + "\n" +
+                            gross + "\n" + 
+                            mc + "\n" + 
+                            name + "\n" + 
+                            phone + "\n" + 
+                            broker + "\n" + 
+                            addInfo;            
+        }
+
+        private void clipBoard_copy_Click(object sender, RoutedEventArgs e)
+        {
+            string text = loadText.Text;
+            Clipboard.SetText(text);
+            this.Close();
+        }
+        
+    }
+}
